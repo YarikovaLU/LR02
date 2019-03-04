3ae2cf945fc89d7b49cdd6d9123571fb92470c56
Luba (YarikovaLU) <luba.rey.l@gmail.com>
2019-03-04 12:44:01 +0300

    Добавление произведения

--- main.cpp
+++ main.cpp
@@ -2,12 +2,14 @@
 
 using namespace std;
 
+// you may type whatever you want
 int main()
 {
     cout << "Enter A and B: ";
     int a, b;
     cin >> a >> b;
     cout << "A + B = " << a + b << '\n'
-         << "A - B = " << a - b << '\n';
+         << "A - B = " << a - b << '\n'
+         << "A * B = " << a * b << '\n';
     return 0;
 }
